bc025bb5e612d301c9633ea7bb7fe0915e98108a
Callum Bondy <callum.bondy@gmail.com>
2025-09-09 16:42:33 -0400

    starting from scratch

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
+    // Uses the APP_KEYS from your .env file (must be at least 2, ideally 4)
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
